eb77a535437bfae9d7393bc47abf074656180439
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-03-10 16:58:18 -0300

    backend tests written and working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,48 +9,60 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
-    beforeAll(async done => {
+
+    beforeAll(async (done) => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
+
+      const signInData = await fakeRequest(app).post('/auth/signup').send({
+        email: 'jon@user.com',
+        password: '1234'
+      });
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
-    afterAll(done => {
+
+    afterAll((done) => {
       return client.end(done);
     });
 
-    test('returns playshare', async() => {
-
+    //GET
+    test('returns shared playlists', async () => {
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          id: 1,
+          name: 'Lo-Fi Beats',
+          uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
+          playlist_id: '09850394',
+          owner_name: 'Zina',
+          owner_id: 1
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          id: 2,
+          name: 'Africano Beats',
+          uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
+          playlist_id: '798435',
+          owner_name: 'Cierra',
+          owner_id: 1
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          id: 3,
+          name: 'Smooth Jazz',
+          uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
+          playlist_id: '974835',
+          owner_name: 'Claudia',
+          owner_id: 1
+        },
+        {
+          id: 4,
+          name: 'Hip hop',
+          uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
+          playlist_id: '97384775',
+          owner_name: 'Katilyn',
+          owner_id: 1
         }
       ];
 
@@ -60,6 +72,43 @@ describe('app routes', () => {
         .expect(200);
 
       expect(data.body).toEqual(expectation);
+    });
+
+    //POST
+    test('adds a new playlist to shared playlist page', async () => {
+      const newPlaylist = {
+        name: 'New Algorhythm Playlist',
+        uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
+        playlist_id: '09850394',
+        owner_name: 'Larry'
+      };
+
+      const addedPlaylist = [
+        {
+          ...newPlaylist,
+          id: 5,
+          owner_id: 2
+        }
+      ];
+
+      const data = await fakeRequest(app)
+        .post('/api/playshare')
+        .send(newPlaylist)
+        .set('Authorization', token)
+        // .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(addedPlaylist);
+    });
+    //POST
+    test('deletes specified playlist given a playlist id (in this case, 5)', async () => {
+      const data = await fakeRequest(app)
+        .delete('/api/playshare/5')
+        .set('Authorization', token)
+        // .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual([]);
     });
   });
 });
--- data/playshare.js
+++ data/playshare.js
@@ -4,27 +4,27 @@ module.exports = [
     uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
     playlist_id: '09850394',
     owner_name: 'Zina',
-    owner_id: 1,
+    owner_id: 1
   },
   {
     name: 'Africano Beats',
     uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
     playlist_id: '798435',
     owner_name: 'Cierra',
-    owner_id: 2
+    owner_id: 1
   },
   {
     name: 'Smooth Jazz',
     uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
     playlist_id: '974835',
     owner_name: 'Claudia',
-    owner_id: 3
+    owner_id: 1
   },
   {
     name: 'Hip hop',
     uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
     playlist_id: '97384775',
     owner_name: 'Katilyn',
-    owner_id: 4
+    owner_id: 1
   }
 ];
