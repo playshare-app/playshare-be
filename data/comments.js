52770507a2b3bf4cb7eb26cdbdbbd10b2c518f4c
Claudia <claudias.mazariegos@gmail.com>
2021-03-11 11:24:17 -0800

    comments backend table done

---
+++ data/comments.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    comment_test: 'test',
+    user_email: 'test',
+    playlistid: 'test',
+    comments_id: 1
+  },
+  {
+    comment_test: 'test',
+    user_email: 'test',
+    playlistid: 'test',
+    comments_id: 1
+  },
+];
