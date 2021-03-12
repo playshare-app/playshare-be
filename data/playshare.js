9f80ec6d316b549a114da2f355ac64d1bc958b6d
cmazariegos44 <41842292+cmazariegos44@users.noreply.github.com>
2021-03-12 08:20:54 -0800

    Update playshare.js

--- data/playshare.js
+++ data/playshare.js
@@ -1,16 +1,32 @@
 module.exports = [
   {
-    name: 'Lo-Fi Beats',
-    uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
-    playlist_id: '09850394',
-    owner_name: 'Zina',
+    name: 'Playlist pop()',
+    uri: 'spotify:user:glass.wavs:playlist:1GsFIL70HSYvHzgTbI8iaM',
+    playlist_id: '1GsFIL70HSYvHzgTbI8iaM',
+    owner_name: 'Dylan White',
     owner_id: 1
   },
   {
-    name: 'Africano Beats',
-    uri: 'spotify:user:wizzler:playlist:53Y8wT46QIMz5H4WQ8O22c',
-    playlist_id: '798435',
-    owner_name: 'Cierra',
+    name: 'searedbird',
+    uri: 'spotify:playlist:0i6MFGB1ZNkP3drrf4aRyu',
+    playlist_id: '0i6MFGB1ZNkP3drrf4aRyu',
+    owner_name: 'Tyler Paul Farris',
     owner_id: 1
-  }
+  },
+  {
+    name: 'pandemic discotheque',
+    uri: 'spotify:playlist:757WAmR3ru4n6JvWGTNqHT',
+    playlist_id: '757WAmR3ru4n6JvWGTNqHT',
+    owner_name: 'Brooke Mundell',
+    owner_id: 1
+  },
+  {
+    name: 'Shitting Rainbows',
+    uri: 'spotify:playlist:02wTKYHA9ap9up2ArVV2Mf',
+    playlist_id: '02wTKYHA9ap9up2ArVV2Mf',
+    owner_name: 'KB',
+    owner_id: 1
+  },
+
+
 ];
